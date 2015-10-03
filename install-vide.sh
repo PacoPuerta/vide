fc643db4dbaa6850ec8a8adc49540c16bb91be86
PacoPuerta <pacopuerta@tiscali.it>
2015-10-03 12:06:46 +0200

    Update install-vide.sh

--- install-vide.sh
+++ install-vide.sh
@@ -7,6 +7,7 @@ curl -LSso ~/.vide/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo
 echo " Installing needed bundles ..."
 cd ~/.vide/bundle
+echo
 git clone https://github.com/vim-scripts/bash-support.vim
 git clone https://github.com/kien/ctrlp.vim
 git clone https://github.com/Yggdroot/indentLine
@@ -19,11 +20,11 @@ git clone https://github.com/vim-scripts/vcscommand.vim
 git clone https://github.com/bling/vim-airline
 git clone https://github.com/jeetsukumaran/vim-buffergator
 git clone https://github.com/bronson/vim-trailing-whitespace
-#TODO bash, maximixe
 
 echo
 echo " Creating symbolic link ..."
 ln -s ~/.vide ~/.vim
 ln -s ~/.vide/.vimrc ~/.vimrc
 
+echo
 echo " Done !"
