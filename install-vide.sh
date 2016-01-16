91dbd5e1f68fc7a2259ad5fcd3aff4bc6cd866be
PacoPuerta <pacopuerta@tiscali.it>
2016-01-16 08:19:29 +0100

    Fix first intall

--- install-vide.sh
+++ install-vide.sh
@@ -4,22 +4,13 @@ echo
 echo " Making ~/.vide dirs and installing pathogen ..."
 mkdir -p ~/.vide/autoload ~/.vide/bundle
 curl -LSso ~/.vide/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 echo
-#echo " Installing needed bundles ..."
-#cd ~/.vide/bundle
-#echo
-#git clone https://github.com/vim-scripts/bash-support.vim
-#git clone https://github.com/kien/ctrlp.vim
-#git clone https://github.com/Yggdroot/indentLine
-#git clone https://github.com/scrooloose/nerdtree
-#git clone https://github.com/ervandew/supertab
-#git clone https://github.com/scrooloose/syntastic
-#git clone https://github.com/vim-scripts/taglist.vim
-#git clone https://github.com/mbbill/undotree
-#git clone https://github.com/vim-scripts/vcscommand.vim
-#git clone https://github.com/bling/vim-airline
-#git clone https://github.com/jeetsukumaran/vim-buffergator
-#git clone https://github.com/bronson/vim-trailing-whitespace
+echo " Installing needed bundles ..."
+cd ~/.vide/
+echo
+git submodule init
+git submodule update
 
 echo
 echo " Creating symbolic link ..."
